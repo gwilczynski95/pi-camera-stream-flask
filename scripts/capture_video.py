d52475c8bdc70f287facc6aa06cf4bad57ffb978
gwilczynski95 <g.wilczynski95@gmail.com>
2023-07-20 18:57:02 +0200

    Add script for capturing video

---
+++ scripts/capture_video.py
@@ -0,0 +1,19 @@
+from picamera import PiCamera
+from time import sleep
+
+
+def main():
+    sleep_time_s = 20
+    out_path = "recording.h264"
+
+    camera = PiCamera()
+
+    camera.start_preview()
+    camera.start_recording(out_path)
+    sleep(sleep_time_s)
+    camera.stop_recording()
+    camera.stop_preview()
+
+
+if __name__ == '__main__':
+    main()
---
+++ scripts/motion_detection.py
@@ -0,0 +1,4 @@
+# Taken from: https://automaticaddison.com/motion-detection-using-opencv-on-raspberry-pi-4/
+
+
+

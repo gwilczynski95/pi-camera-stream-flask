f321dda23ad8ada7bb3d2118e0fc1bdd52487551
gwilczynski95 <g.wilczynski95@gmail.com>
2023-07-20 20:57:00 +0200

    Add setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pi-camera-stream-flask',
+    version='0.0.1',
+    packages=['scripts'],
+    url='',
+    license='',
+    author='g.wilczynski95',
+    author_email='',
+    description=''
+)

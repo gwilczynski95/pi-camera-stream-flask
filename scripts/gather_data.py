b2c73fae5b4a241c81d197067b2fd0a35cd0b793
gwilczynski95 <g.wilczynski95@gmail.com>
2023-07-23 16:14:51 +0200

    Update gathering script

--- scripts/gather_data.py
+++ scripts/gather_data.py
@@ -20,7 +20,7 @@ def _load_yaml(path):
 
 
 def current_time(tz="Europe/Warsaw"):
-    return datetime.now(pytz.timezone(tz))
+    return datetime.datetime.now(pytz.timezone(tz))
 
 
 def create_detector(detector_cfg: dict):
@@ -44,32 +44,32 @@ def init_camera(config: dict):
     )
     raw_capture = PiRGBArray(
         camera,
-        size=config["resulotion"]
+        size=config["resolution"]
     )
     return camera, raw_capture
 
 
-def create_out_filename(extension, seconds_before, strf="%Y-%m-%d_%H:%M:%S"):
+def create_out_filename(output_path, extension, seconds_before, strf="%Y-%m-%d_%H:%M:%S"):
     curr_datetime = current_time() - datetime.timedelta(seconds=seconds_before)
     datetime_str = str(curr_datetime.strftime(strf))
-    return f"{datetime_str}{extension}"
+    return str(Path(output_path, f"{datetime_str}{extension}"))
 
 
 def main(config: dict):
-    output_path = config["output_path"]
+    output_path = Path(config["output_path"])
     output_path.mkdir(parents=True, exist_ok=True)
 
     # init detector and camera
     detector = create_detector(config["detection_algorithm"])
 
-    frames_before = int(config["seconds_before"] / 60. * config["frame_rate"])
+    frames_before = int(config["seconds_before"] / 60. * config["frame_rate"])  # todo: this is bad, fix this
     frames_after = int(config["seconds_after"] / 60. * config["frame_rate"])
 
     before_buffer = np.zeros(
-        (frames_before,) + config["resolution"] + (3,), dtype=np.uint8
+        [frames_before] + config["resolution"][::-1] + [3], dtype=np.uint8
     )
 
-    fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 
     camera, raw_capture = init_camera(config)
 
@@ -92,30 +92,34 @@ def main(config: dict):
                 if not clip_started:
                     clip_started = True
                     filename = create_out_filename(
+                        config["output_path"],
                         config["video_extension"],
                         config["seconds_before"]
                     )
                     clip_obj = cv2.VideoWriter(
                         filename,
-                        fourcc,
-                        config["frame_rate"],
-                        config["resolution"]
+                        fourcc=fourcc,
+                        fps=config["frame_rate"],
+                        frameSize=config["resolution"],
                     )
+                    print("Started recording clip")
                     for _frame in before_buffer:
-                        clip_obj.write(frame)
+                        clip_obj.write(_frame)
                 in_after_buffer_state = frames_after
                 clip_obj.write(frame)
             elif not is_detected and in_after_buffer_state >= 0 and clip_started:
                 in_after_buffer_state -= 1
-                clip_obj.append(frame)
+                clip_obj.write(frame)
                 if in_after_buffer_state == -1:
                     clip_started = False
                     clip_obj.release()
+                    print("Finished clip")
             else:
                 before_buffer = np.roll(before_buffer, 1, axis=0)
                 before_buffer[
-                    in_bf_buff_state, :, :, :
+                    -1, :, :, :
                 ] = frame
+        raw_capture.truncate(0)
 
 
 if __name__ == '__main__':
